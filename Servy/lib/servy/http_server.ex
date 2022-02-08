6b66ca9c6f9e02a7edaff1b406d7a679473bd621
Matan Amidor <87947521+Mado13@users.noreply.github.com>
2022-02-08 11:37:12 +0100

    added concurrency and fault tolerence to the httpserver.ex

--- Servy/lib/servy/http_server.ex
+++ Servy/lib/servy/http_server.ex
@@ -10,11 +10,12 @@ defmodule Servy.HttpServer do
     IO.puts "Waiting to accept a client connection...\n"
     {:ok, client_socket} = :gen_tcp.accept(listen_socket)
     IO.puts "Connection Accepted!\n"
-    serve(client_socket)
+    spawn(fn -> serve(client_socket) end)
     accept_loop(listen_socket)
   end
 
   def serve(client_socket) do
+    IO.puts "#{inspect self()}: Working on it!"
     client_socket
       |> read_request
       |> Servy.Handler.handle()
@@ -29,16 +30,6 @@ defmodule Servy.HttpServer do
     request
   end
 
-  def generate_response(_request) do
-    """
-      HTTP/1.1 200 OK\r
-      Content-Type: text/plain\r
-      Content-Length: 6\r
-      \r
-      Hello!
-    """
-  end
-
   def write_response(response, client_socket) do
     :ok = :gen_tcp.send(client_socket, response)
 
@@ -47,5 +38,4 @@ defmodule Servy.HttpServer do
 
     :gen_tcp.close(client_socket)
   end
-
 end
