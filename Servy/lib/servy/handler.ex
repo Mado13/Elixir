acff54852cd62c9f4881555a1bf82cb3c5653bd4
Matan Amidor <87947521+Mado13@users.noreply.github.com>
2022-02-08 16:25:32 +0100

    Introduced Tasks to handle concurrency request to our web server

--- Servy/lib/servy/handler.ex
+++ Servy/lib/servy/handler.ex
@@ -4,6 +4,7 @@ defmodule Servy.Handler do
 
   alias Servy.Conv
   alias Servy.BearController
+  alias Servy.VideoCam
 
   @pages_path Path.expand("../../pages", __DIR__)
 
@@ -19,6 +20,19 @@ defmodule Servy.Handler do
     |> route
     |> track
     |> format_response
+  end
+
+  def route(%Conv{ method: "GET", path: "/sensors" } = conv) do
+    task = Task.async((fn -> Servy.Tracker.get_location("bigfoot") end))
+
+    snapshots =
+      ["cam-1", "cam-2", "cam-3"]
+      |> Enum.map(&Task.async(fn -> VideoCam.get_snapshot(&1) end))
+      |> Enum.map(&Task.await/1)
+
+     where_is_bigfoot = Task.await(task)
+
+    %{ conv | status: 200, resp_body: inspect {snapshots, where_is_bigfoot} }
   end
 
   def route(%Conv{ method: "GET", path: "/wildthings" } = conv) do
